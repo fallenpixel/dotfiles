0b366cf0d0ff4352645dcfab68aa98cfa794dd13
Eric Lehmann <katyl@katyl.info>
2021-05-25 15:26:34 -0400

    Update

--- ssh/.config/environment.d/ssh_env.sh
+++ ssh/.config/environment.d/ssh_env.sh
@@ -1,6 +1,1 @@
-#!/bin/sh
-#export SSH_ASKPASS=/usr/bin/ksshaskpass
-#exec '/usr/bin/ssh-add' $HOME/.ssh/digitalocean_rsa $HOME/.ssh/git_rsa $HOME/.ssh/id_ed25519 $HOME/.ssh/inf_rsa $HOME/.ssh/personal_rsa </dev/null
-#exit
-export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-
+export SSH_AUTH_SOCK="$( gpgconf --list-dirs agent-ssh-socket )"
