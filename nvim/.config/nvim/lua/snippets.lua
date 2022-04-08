485f7860729d15ae3cb2ec252c50ef9e43f937c4
Eric Lehmann <katyl@katyl.info>
2022-04-08 07:10:02 -0400

    Updates

--- nvim/.config/nvim/lua/snippets.lua
+++ nvim/.config/nvim/lua/snippets.lua
@@ -1,7 +1,8 @@
 local luasnip_status, luasnip = pcall(require, 'luasnip')
+local cmp_status, cmp = pcall(require, "cmp")
 local lsextras_status, extras = pcall(require, 'luasnip.extras')
 local lsfmt_status, format = pcall(require, 'luasnip.extras.fmt')
-if not (luasnip_status and lsextras_status and lsfmt_status) then
+if not (luasnip_status and lsextras_status and lsfmt_status and cmp_status) then
   print("failed")
   return
 else
@@ -15,16 +16,14 @@ else
   local dynamic = luasnip.dynamic_node
   local fmt = format.fmt
   local rep = extras.rep
+
   luasnip.snippets = {
     ansible = {
       snip("lookup-local", fmt("\"{{{{ lookup(\'env\', \'{}\' }}}}\"", { insert(1,"HOME") })),
       snip("lookup-remote", fmt("\"{{{{ ansible_env.{} }}}}\"", { insert(1,"HOME") }))
     },
     lua = {
-      snip("pcall", fmt("local {}_status, {} = pcall(require, '{}')\nif {}{}_status then\n\treturn\nend",
-     { rep(1), insert(1), rep(1), choice(2, {text(" "), text("not ")}), rep(1)})),
-      snip("use", fmt("use {{\n\t'{}',\n\tconfig = {{\n\t\tfunction()\n\t\tlocal {} = require('{}').setup{{\n\t\t\t{}\n\t\t}}\n\t}}\n}}",
-      {insert(1), insert(2), rep(2), insert(3)}))
+      snip("test", fmt("foo {}", insert(1)))
     }
   }
 end
