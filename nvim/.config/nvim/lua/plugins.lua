9ce0ba29290852a211d1f2025ed225095722f993
Eric Lehmann <katyl@katyl.info>
2022-05-09 21:34:53 -0400

    Floating neogit

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -32,6 +32,9 @@ return require('packer').startup({function(use)
     requires = 'nvim-lua/plenary.nvim',
     config = function()
       vim.keymap.set('n', '<leader>gg', '<cmd>Neogit<CR>')
+      require('neogit').setup {
+      kind = "floating"
+      }
     end
   }
   use {
