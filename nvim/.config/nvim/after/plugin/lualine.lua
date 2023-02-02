3cfadac2d7b37c005881f44fdeaaaddc6cbe3504
Eric Lehmann <katyl@katyl.info>
2023-02-02 08:37:49 -0500

    Update yaml mappings

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -1,3 +1,10 @@
+local function get_schema()
+  local schema = require("yaml-companion").get_buf_schema(0)
+  if schema.result[1].name == "none" then
+    return ""
+  end
+  return schema.result[1].name
+end
 local lualine = require("lualine")
 lualine.setup {
   options = {
@@ -7,6 +14,7 @@ lualine.setup {
   },
   sections = {
       lualine_x = {
+          get_schema,
           'encoding',
           'fileformat',
           'filetype'
